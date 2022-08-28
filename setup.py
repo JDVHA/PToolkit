b94e0c7b170ccc24053bd03bda0976be8985ae0a
Junior <jdvha@outlook.com>
2022-08-28 16:23:38 +0200

    Pypi release

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+from gettext import install
 from setuptools import setup
 
 
 setup(
     name="PToolkit",
-    version="0.1",
-    description="",
-    url="#",
+    version="1.0",
+    description="A set of tools than is usefull in lots of diffrent fields. This toolkit contains functions to make professional looking matplotlib plot without spending time on the looks of the plot. Not only that this toolkit also provides functions for rounding and determining errors.",
+    url="https://github.com/JDVHA/PToolkit",
     author="H.A.J de Vries",
     author_email="",
-    license="MIT"
+    license="MIT",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "sympy"
+    ]
 )
 
