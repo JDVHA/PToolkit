2bdc913689b8c8b0a5d5ac402030352f964cef6d
Junior <jdvha@outlook.com>
2024-09-25 14:55:41 +0200

    u

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 version = "2.0.0"
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name="PToolkit",
     version=version,
     description="A set of tools to make working in a lab easier.",
     long_description_content_type='text/markdown',
-    long_description="A set of tools to make working in a lab easier.",
+    long_description="README",
     url="https://github.com/JDVHA/PToolkit",
     author="H.A.J de Vries",
     author_email="",
